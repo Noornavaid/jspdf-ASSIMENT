e73a66187637b27ea2d0b3bb1160085049bf2920
Noornavaid <154305847+Noornavaid@users.noreply.github.com>
2024-03-23 09:27:07 +0500

    SIR SAHZAD PDF ASSIMENT JS

---
+++ index.js
@@ -0,0 +1,30 @@
+// question 1
+//let username;
+
+// question 2
+let myName = "noor Ahmed";
+
+// question 3
+let message = "Hello world";
+alert(message);
+
+let username = prompt("what is your name?");
+alert(username);
+
+alert("BISSCUT\nBISSCU\nBISSC\nBISSC\nBISS");
+
+// 6
+let email = "example@gmail.com";
+alert(" UFF email is " + email);
+
+/* Question 7 */
+let book =
+  "i am trying to learn from the Book A smarter\nway to learn JavaScript.";
+alert(book);
+
+/* Question 8 */
+document.write("Yah! I can write HTML content through Javascript");
+
+/* Question 9 */
+let lines ;
+alert(lines);
